5b3f15a22d4eaad9fc6e2bad12d532d94f448899
Luis Fax <30512732+LuisFaax@users.noreply.github.com>
2017-08-31 13:51:41 -0500

    Update concerts.js

--- routes/concerts.js
+++ routes/concerts.js
@@ -4,7 +4,10 @@ const Concert = require('../controllers/controllerConcerts')
 const auth = require('../middlewares/auth')
 const token = require('../services/token')
 
-
+//create a welcome route
+app.get('/', (req, res) => {
+    res.send({message: "Welcome to Concerts RESTful API,  ¡¡Have fun!! "})
+});
 
 //retrieve all concert
 app.get('/concerts',  Concert.getAll)
